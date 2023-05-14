3bdb64e18af053e091debe31e1ca66c784cb6c43
vboechat <victor2005contato@gmail.com>
2023-05-14 13:06:16 -0300

    chore: add baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "o32t3c",
   e2e: {
+    baseUrl: "http://localhost:4173",
     chromeWebSecurity: false,
     specPattern: "./src/**/*.cy-e2e.{ts,tsx}",
   },
